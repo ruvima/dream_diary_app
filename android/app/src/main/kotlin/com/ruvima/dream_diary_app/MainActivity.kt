dd78df94060d4867017cb2dd90a72315ab07b71a
Victor Ruiz <vmanr3@gmail.com>
2023-10-15 22:34:38 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/ruvima/dream_diary_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ruvima.dream_diary_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/screens/dream_diary/dream_diary_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dream Diary App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DreamDiary(),
+    );
+  }
+}
