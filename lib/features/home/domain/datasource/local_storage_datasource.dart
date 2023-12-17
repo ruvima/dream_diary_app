06e89df96b5bf8a46c16d30476c3938f86b4b3bd
Victor Ruiz <vmanr3@gmail.com>
2023-12-17 00:06:52 -0600

    isar database

---
+++ lib/features/home/domain/datasource/local_storage_datasource.dart
@@ -0,0 +1,11 @@
+import '../domain.dart';
+
+abstract class LocalStorageDatasource {
+  Future<List<DreamEntity>> getDreams();
+
+  Future<bool> create(DreamEntity dream);
+
+  Future<bool> update(DreamEntity dream);
+
+  Future<bool> delete(int id);
+}
