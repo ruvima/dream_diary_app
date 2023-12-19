280467e8ae8ddb38475ae576398a4377e6dea13e
Victor Ruiz <vmanr3@gmail.com>
2023-12-19 01:15:30 -0600

    added more options

--- lib/core/widgets/k_icon_button.dart
+++ lib/core/widgets/k_icon_button.dart
@@ -4,22 +4,30 @@ class KIconButton extends StatelessWidget {
   const KIconButton({
     super.key,
     required this.icon,
-    this.compact = false,
     required this.onPressed,
+    this.compact = false,
+    this.padding,
+    this.size,
   });
-  final IconData icon;
   final bool compact;
+  final double? size;
+  final EdgeInsetsGeometry? padding;
   final Function()? onPressed;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: onPressed,
       color: Theme.of(context).colorScheme.primary,
+      padding: padding,
       visualDensity: compact
           ? const VisualDensity(horizontal: -2, vertical: -2)
           : VisualDensity.standard,
-      icon: Icon(icon),
+      icon: Icon(
+        icon,
+        size: size,
+      ),
     );
   }
 }
